4c25c75b31666c8642a7726a27a043f7175ec029
Nguyen Cao <nnp.cao@gmail.com>
2015-11-24 20:57:50 +0100

    Translate to vietnamese

--- script.js
+++ script.js
@@ -5,16 +5,129 @@ var images =
 		"psg.png"
 	];
 
-// qd la page est chargée("ready"), ça cache les 2 suivants
-$( document ).ready(function() {	
+var text = {};
+text["fr"] = {};
+text["fr"]["txt-title"] = "Hanh Phuc Sport - Boutique de sport - My Tho";
+text["fr"]["txt-home"] = "Accueil";
+text["fr"]["txt-products"] = "Nos produits";
+text["fr"]["txt-findus"] = "Nous trouver";
+text["fr"]["txt-services"] = "Nos prestations";
+text["fr"]["txt-footer"] = "Tel: Mme. Huong: +84.912.219.500 - M. Nghiep: +84.918.477.457	/ 	Email: ngcamhuong@yahoo.com";
+text["fr"]["txt-football-content"] = "Maillots, Chaussures, Balles, Brassard, Filet de but, ...";
+text["fr"]["txt-accessory-content"] = "Chaussures (de course, danse)<br>Sac de sport, à dos";
 
+text["vn"] = {};
+text["vn"]["txt-title"] = "Hạnh Phúc Sport - Cửa hàng TDTT - Mỹ Tho";
+text["vn"]["txt-home"] = "Trang chủ";
+text["vn"]["txt-products"] = "Sản Phẩm";
+text["vn"]["txt-findus"] = "Địa chỉ liên hệ";
+text["vn"]["txt-footer"] = "Liên hệ: Hương: +84.912.219.500 - Nghiệp: +84.918.477.457	/ 	Email: ngcamhuong@yahoo.com";
+text["vn"]["txt-services"] = "Dịch vụ";
+text["vn"]["txt-football"] = "Bóng đá";
+text["vn"]["txt-football-content"] = "Áo CLB, Giày, Banh, Lưới<br>Băng đội trưởng Lưới ...";
+text["vn"]["txt-fitness"] = "Thể dục bụng";
+text["vn"]["txt-fitness-content"] = "Máy tập xoay eo<br>Máy tập tình yêu";
+text["vn"]["txt-accessory"] = "Giày,Túi xách thể thao";
+text["vn"]["txt-accessory-content"] = "Giày thể thao<br>(thời trang, chạy bộ, múa)<br>Túi xách Ba lô";
+text["vn"]["txt-tennis"] = "Quần vợt";
+text["vn"]["txt-tennis-content"] = "Áo Quần vợt<br>Vợt<br>Banh";
+text["vn"]["txt-body"] = "Tập thể hình";
+text["vn"]["txt-body-content"] = "Tạ Tay<br>Kìm bóp tay<br>Giá Treo<br>Ghế đẩy tạ";
+text["vn"]["txt-combat"] = "Võ thuật thể lực";
+text["vn"]["txt-combat-content"] = "Găng Đấm<br>Bao cát";
+text["vn"]["txt-yoga-content"] = "Thảm tập<br>Bóng Yoga";
+text["vn"]["txt-badminton"] = "Cầu lông<br>Cầu đá";
+text["vn"]["txt-badminton-content"] = "Áo cầu lông<br>Vợt cầu lông<br>Cầu lông, Cầu đá";
+text["vn"]["txt-basketball"] = "Bóng rổ";
+text["vn"]["txt-basketball-content"] = "Quả bóng rổ<br>Trụ, vành & lưới bóng rổ";
+text["vn"]["txt-swim"] = "Bơi lội";
+text["vn"]["txt-swim-content"] = "Áo bơi<br>Kính bơi<br>Mũ bơi";
+text["vn"]["txt-service"] = "DỊCH VỤ IN ẤN TRÊN MỌI CHẤT LIỆU";
+text["vn"]["txt-service-content"] = " Với công nghệ in ÁO BÓNG ĐÁ là hệ thống in sơn (in lụa) chuyên nghiệp, chúng tôi đảm bảo giá cả phải chăng, chất lượng sơn không phai màu. Ngoài ra chúng tôi còn nhận in các sản phẩm với nhiều chất liệu khác với công nghệ IN OFFSET:<br><br>- Bao bì sản phẩm (ni lông, giấy , da , vải) <br>- Danh thiếp, Thiệp cưới, Thiệp mời, Bao thư <br> - Hóa đơn, Vé giữ xe, Vé hội chợ. ";
+text["vn"]["txt-address-title"] = " Địa chỉ";
+text["vn"]["txt-address"] = "12A1, đường Lý Thường Kiệt, P. 5<br>TP. Mỹ Tho, Tỉnh Tiền Giang";
+text["vn"]["txt-phone-title"] = "Điện thoại";
+
+text["en"] = {};
+
+
+function SetLang(lang)
+{
+	var firstTime = Object.keys(text["en"]).length == 0;
+
+	var elements = $("[id^=txt]");
+	for(var i = 0 ; i < elements.length ; i++)
+	{
+		var e = elements[i];
+		if(text[lang][e.id])
+		{
+			if(firstTime)
+				text["en"][e.id] = e.innerHTML;
+			e.innerHTML = text[lang][e.id];
+		}
+	}
+
+	var links = $("#menu a[href^=#]");
+	for(var i = 0 ; i < links.length ; i++)
+	{
+		var e = links[i];
+		var elink = $(e).attr("href").split("#")[1];
+		var original = elink.split('-');
+		if(original.length > 1)
+			original = original[1];
+		else
+			original = original[0];
+		
+		$(e).attr("href", "#" + lang + "-" + original);
+	}
+}
+
+function GetLang()
+{
+	var lang = ["fr", "en"];
+
+	var tbl = document.location.hash.split("#");
+	if(tbl.length < 2) 
+		return "vn";
+
+	tbl = tbl[1].split("-");
+	
+	if(tbl.length < 1)
+		return "vn";
+
+	var index = lang.indexOf(tbl[0]);
+	if(index >= 0)
+		return lang[index];
+	else
+		return "vn";
+}
+
+function GetPage()
+{
+	var tbl = document.location.hash.split("#");
+	if(tbl.length < 2) 
+		return "#home";
+
+	tbl = tbl[1].split("-");
+	
+	if(tbl.length < 1)
+		return "#home";
+
+	if($("#" + tbl[tbl.length-1]).length)
+		return "#" + tbl[tbl.length-1];
+	
+	return "#home";
+}
+
+// qd la page est chargée("ready"), ça cache les 2 suivants
+$( document ).ready(function() 
+{	
+	SetLang(GetLang());
+	
 	$("#content").children().hide();
 	$("#content").children().css("visibility", "visible");
 		
-	if($(document.location.hash).length)
-		$(document.location.hash).show();
-	else
-		$("#home").show();
+	$(GetPage()).show();
 	
 	for(var index = 0 ; index < images.length; index++)
 	{
@@ -28,7 +141,10 @@ $( document ).ready(function() {
 		event.preventDefault();
 		document.location.hash = $(this).attr("href");
 		$("#content").children().fadeOut();
-		$($(this).attr("href")).fadeIn();
+
+		SetLang(GetLang());
+		$(GetPage()).fadeIn();
+
 
 		// if responsive, hide menu
 		if($("#menu-icon").css("width") != "0px") {
